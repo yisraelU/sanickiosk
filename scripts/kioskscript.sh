55a92dab8be98b7a5d39c771e9849c328bdb461c
Scott Sanicki <scott@sanicki.com>
2015-08-19 11:11:43 -0700

    More fixes for logs (test)

--- scripts/kioskscript.sh
+++ scripts/kioskscript.sh
@@ -26,10 +26,10 @@ clear
 
 # Set Log
 mkdir $install_dir/logs && touch $install_dir/logs/kioskscript.log # Create log directory and file
-log_it=">> /dev/null 2>> $install_dir/logs/kioskscript.log"
+#log_it=">> /dev/null 2>> $install_dir/logs/kioskscript.log"
 
 # Make empty directories
-mkdir $install_dir/screensavers $log_it
+mkdir $install_dir/screensavers >> /dev/null 2>> $install_dir/logs/kioskscript.log
 
 # Pretty colors
 red='\e[1;31m'
@@ -38,7 +38,7 @@ yellow='\e[0;33m'
 nc='\e[0m' # No color
 
 # Prevent terminal blanking
-setterm -powersave off -blank 0 $log_it
+setterm -powersave off -blank 0 >> /dev/null 2>> $install_dir/logs/kioskscript.log
 
 echo -e "${red}Installing operating system updates ${yellow}(this may take a while)${red}...${nc}"
 # Use mirror method
@@ -49,30 +49,30 @@ deb mirror://mirrors.ubuntu.com/mirrors.txt $version-backports main restricted u
 deb mirror://mirrors.ubuntu.com/mirrors.txt $version-security main restricted universe multiverse\n\
 " /etc/apt/sources.list > /dev/null
 # Refresh
-apt-get -q update $log_it
+apt-get -q update >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Download & Install
-apt-get -q upgrade $log_it
+apt-get -q upgrade >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Clean
-apt-get -q autoremove $log_it
-apt-get -q clean $log_it
+apt-get -q autoremove >> /dev/null 2>> $install_dir/logs/kioskscript.log
+apt-get -q clean >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Installing software ${yellow}(this will take a while)${red}...${nc}"
 # Ajenti
-wget -q http://repo.ajenti.org/debian/key -O- | apt-key add - $log_it
+wget -q http://repo.ajenti.org/debian/key -O- | apt-key add - >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo '
 deb http://repo.ajenti.org/ng/debian main main ubuntu
-'  >> /etc/apt/sources.list.d/ajenti.list $log_it
+'  >> /etc/apt/sources.list.d/ajenti.list >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Systemback
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 73C62A1B $log_it
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 73C62A1B >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "
 deb http://ppa.launchpad.net/nemh/systemback/ubuntu $version main
-"  >> /etc/apt/sources.list.d/systemback.list $log_it
+"  >> /etc/apt/sources.list.d/systemback.list >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Flash
 echo -e "
 deb http://archive.canonical.com/ubuntu/ $version partner
-"  >> /etc/apt/sources.list.d/canonical_partner.list $log_it
-apt-get -q update $log_it
+"  >> /etc/apt/sources.list.d/canonical_partner.list >> /dev/null 2>> $install_dir/logs/kioskscript.log
+apt-get -q update >> /dev/null 2>> $install_dir/logs/kioskscript.log
 packagelist=(
   alsa # Audio
   ajenti # Browser-based system administration tool
@@ -88,25 +88,25 @@ packagelist=(
   software-properties-common python-software-properties # Enable PPA installs
   systemback-cli # Systemback custom image maker
 )
-apt-get -q install --no-install-recommends ${packagelist[@]} $log_it
-tasksel install print-server $log_it
+apt-get -q install --no-install-recommends ${packagelist[@]} >> /dev/null 2>> $install_dir/logs/kioskscript.log
+tasksel install print-server >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Disabling root recovery mode...${nc}"
-sed -i -e 's/#GRUB_DISABLE_RECOVERY/GRUB_DISABLE_RECOVERY/g' /etc/default/grub $log_it
-update-grub $log_it
+sed -i -e 's/#GRUB_DISABLE_RECOVERY/GRUB_DISABLE_RECOVERY/g' /etc/default/grub >> /dev/null 2>> $install_dir/logs/kioskscript.log
+update-grub >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Configuring autologin...${nc}"
-sed -i -e 's/NODM_ENABLED=false/NODM_ENABLED=true/g' /etc/default/nodm $log_it
-sed -i -e 's/NODM_USER=root/NODM_USER=sanickiosk/g' /etc/default/nodm $log_it
+sed -i -e 's/NODM_ENABLED=false/NODM_ENABLED=true/g' /etc/default/nodm >> /dev/null 2>> $install_dir/logs/kioskscript.log
+sed -i -e 's/NODM_USER=root/NODM_USER=sanickiosk/g' /etc/default/nodm >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Configuring the screensaver...${nc}"
 # Link .xscreensaver
-ln -s $install_dir/xscreensaver $home_dir/.xscreensaver $log_it
+ln -s $install_dir/xscreensaver $home_dir/.xscreensaver >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Add a sample image
-wget -q http://beginwithsoftware.com/wallpapers/archive/Various/images/free_desktop_wallpaper_logo_space_for_rent_1024x768.gif -O $install_dir/screensavers/deleteme.gif $log_it
+wget -q http://beginwithsoftware.com/wallpapers/archive/Various/images/free_desktop_wallpaper_logo_space_for_rent_1024x768.gif -O $install_dir/screensavers/deleteme.gif >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Configuring the browser ${yellow}(Firefox)${red}...${nc}"
@@ -122,31 +122,31 @@ echo -e "${green}Done!${nc}"
 
 echo -e "${red}Setting up the SanicKiosk scripts...${nc}"
 # Link .xsession
-ln -s $install_dir/xsession $home_dir/.xsession $log_it
+ln -s $install_dir/xsession $home_dir/.xsession >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Set correct user and group permissions for /home/kiosk
-chown -R $user:$user $home_dir $log_it
+chown -R $user:$user $home_dir >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Set scripts to exexutable
-find $install_dir/scripts -type f -exec chmod +x {} \; $log_it
+find $install_dir/scripts -type f -exec chmod +x {} \; >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Configuring the browser-based system administration tool ${yellow}(Ajenti)${red}...${nc}"
-service ajenti stop $log_it
+service ajenti stop >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Changing to default https port
-sed -i 's/"port": 8000/"port": 443/' /etc/ajenti/config.json $log_it
+sed -i 's/"port": 8000/"port": 443/' /etc/ajenti/config.json >> /dev/null 2>> $install_dir/logs/kioskscript.log
 # Linking SanicKiosk plugins to Ajenti
-ln -s $install_dir/ajenti_plugins/sanickiosk_browser /var/lib/ajenti/plugins/sanickiosk_browser $log_it
-ln -s $install_dir/ajenti_plugins/sanickiosk_screensaver /var/lib/ajenti/plugins/sanickiosk_screensaver $log_it
+ln -s $install_dir/ajenti_plugins/sanickiosk_browser /var/lib/ajenti/plugins/sanickiosk_browser >> /dev/null 2>> $install_dir/logs/kioskscript.log
+ln -s $install_dir/ajenti_plugins/sanickiosk_screensaver /var/lib/ajenti/plugins/sanickiosk_screensaver >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Enabling audio...${nc}"
-adduser $user audio $log_it
+adduser $user audio >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Setting up print server...${nc}"
-usermod -aG lpadmin $user $log_it
-usermod -aG lp,sys $user $log_it
-rm -f /etc/cups/cupsd.conf $log_it
-ln -s $install_dir/etc/cups/cupsd.conf /etc/cups/cupsd.conf $log_it
+usermod -aG lpadmin $user >> /dev/null 2>> $install_dir/logs/kioskscript.log
+usermod -aG lp,sys $user >> /dev/null 2>> $install_dir/logs/kioskscript.log
+rm -f /etc/cups/cupsd.conf >> /dev/null 2>> $install_dir/logs/kioskscript.log
+ln -s $install_dir/etc/cups/cupsd.conf /etc/cups/cupsd.conf >> /dev/null 2>> $install_dir/logs/kioskscript.log
 echo -e "${green}Done!${nc}"
 
 echo -e "${red}Locking down the SanicKiosk user...${nc}"
