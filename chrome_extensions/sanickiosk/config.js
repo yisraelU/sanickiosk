7824cbd203491183b3515ad8e360d657193f1948
Scott Sanicki <scott@sanicki.com>
2015-08-20 07:20:16 -0700

    Adding SanicKiosk extension

--- chrome_extensions/sanickiosk/config.js
+++ chrome_extensions/sanickiosk/config.js
@@ -2,9 +2,10 @@ window.config = {
    "model_": "AppConfig",
    "id": 1,
    "appName": "Sanickiosk",
-   "homepage": "sanickiosk.org",
-   "enableLogoutBttn": false,
+   "homepage": "http://sanickiosk.org",
+   "enableLogoutBttn": true,
    "sessionDataTimeoutTime": 5,
    "sessionTimeoutTime": 3,
+   "rotation": "0",
    "kioskEnabled": true
 };
