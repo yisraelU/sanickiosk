f795ae519d64f8587b379c0fd26ce3d783e9ae91
Scott Sanicki <scott@sanicki.com>
2015-08-21 11:17:07 -0700

    Trying to launch SanicKiosk Chrome app using a hack, part 8

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get SanicKiosk extension app id
+str=`cat .config/google-chrome/Default/Preferences` # Read file as string
+IFS=’\"’ read -ra blocks <<< "$str" echo ${blocks[1]} # Convert string to array
+# As function in case needed again
+find_app_id () {
+	for i in "${!blocks[@]}"; do
+		 if [[ "${blocks[$i]}" = "${1}" ]]; then
+				 index=${i}
+		 fi
+	done
+	index=`expr $index - 6` # The app id is 6 delimeters prior to the app name
+	app_id=${blocks[$index]}
+	echo $app_id
+}
+# Call function
+SanicKiosk_app_id=`find_app_id 'SanicKiosk'`
+echo $SanicKiosk_app_id
